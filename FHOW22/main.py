a4c1fafa4a7967826401d308768bdef02c4a4676
Sinyu Jeong <shsy0404g@gmail.com>
2022-08-26 06:26:22 +0900

    Update main.py

--- FHOW22/main.py
+++ FHOW22/main.py
@@ -3,53 +3,57 @@ import argparse
 from pathlib import Path
 from shutil import copyfile
 
-import yaml
+import wandb
 import torch
 from box import Box
-from utils import validate, fix_seed, add_fold
-from loader import get_loader
+from loguru import logger
+
 from solver import Solver
+from loader import add_fold, get_loader
+from utils import Validator, fix_seed, get_config
 
 
-PFX = "***** "
+def main(cfg:Box):
+    logger.add(f"{cfg.base.save_dir}/log")
+    logger.info(f"Config : {dict(cfg)}")
 
-def main(cfg):
-    # validate(cfg)
-    fix_seed(cfg.seed)
-
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(cfg.gpu_number)
-
+    Validator(cfg).main()
+    fix_seed(cfg.base.seed)
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(cfg.base.gpu_number)
     if torch.cuda.is_available():
-        print(f"{PFX}Use {torch.cuda.get_device_name()}")
-
-    save_dir = Path(cfg.save_dir)
-    if not save_dir.exists():
-        save_dir.mkdir()
-        Path(save_dir / "model").mkdir()
-        print(f"{PFX}Make {save_dir.as_posix()}")
-        time.sleep(1)
+        logger.info(f"Use {torch.cuda.get_device_name()}")
+    
+    save_dir = Path(cfg.base.save_dir)
+    model_dir = Path(save_dir) / "model"
+    
+    save_dir.mkdir(parents=True, exist_ok=True)
+    logger.info(f"Build {save_dir.as_posix()} directory")
+    model_dir.mkdir(parents=True, exist_ok=True)
+    logger.info(f"Build {model_dir.as_posix()} directory")
+    time.sleep(1)
 
     copyfile("./config.yaml", save_dir / "config.yaml")
-    data = add_fold(cfg)
+    logger.info(f"Copy config file")
 
+    data = add_fold(cfg.data.data_dir)
+    
     if cfg.train.n_fold == 1:
-        print(f"{PFX}HOLDOUT")
-        train_loader, valid_loader, _ = get_loader(data, cfg.data, None)
+        logger.info(f"HOLDOUT train start")
+        train_loader, valid_loader, _ = get_loader(data, cfg, None)
         solver = Solver(cfg, (train_loader, valid_loader), None)
-        best = solver.train()
-        print(f"{PFX}Best Score: {best}")
-
+        solver.train()
+        
     # else:
     #     bests = []
     #     for fold in range(1, cfg.train.n_fold+1):
-    #         print(f"{PFX}FOLD {fold}")
     #         train_loader, valid_loader = get_loader(fold)
     #         model = get_model()
     #         solver = Solver()
     #         bests.append(solver.train())
-
-    #     print(f"{PFX}bests")
-    #     print(f"{PFX}Best Avg Score: {sum(bests)/fold}")
+    # 
+    # test_loader = get_loader()
+    # solver = Solver(cfg, test_loader)
+    # solver.inference()
 
 
 if __name__ == "__main__":
@@ -57,8 +61,15 @@ if __name__ == "__main__":
     parser.add_argument("--config", type=str, default="./config.yaml")
     args = parser.parse_args()
 
-    with open(args.config) as f:
-        config = Box(yaml.load(f, Loader=yaml.FullLoader))
+    config = get_config(args.config)
 
-    print(config)
-    main(config)
+    if config.log.wandb:
+        wandb.init(
+            project=config.log.project,
+            entity=config.log.entity,
+            name=config.log.name,
+            config=dict(config)
+        )
+        main(config)
+    else:
+        main(config)
