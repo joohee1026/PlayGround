97f09401fb4e326c1627f2aa341242e8a3ca5288
Sinyu Jeong <shsy0404g@gmail.com>
2022-08-26 06:26:35 +0900

    Update model.py

--- FHOW22/model.py
+++ FHOW22/model.py
@@ -2,22 +2,22 @@ import torch.nn as nn
 from timm import create_model
 
 
-def get_model(model_name="convnext_base_384_in22ft1k"):
-    return Model(model_name)
+def get_model(model_name:str, pretrained:bool=True):
+    return Model(model_name, pretrained)
 
 
 class Model(nn.Module):
-    def __init__(self, model_name):
-        super().__init__()
-        self.base = create_model(model_name, pretrained=True)
-
+    def __init__(self, model_name:str, pretrained:bool):
+        super().__init__()    
+        self.base = create_model(model_name, pretrained=pretrained)
+    
         in_feats = self.base.get_classifier().in_features
         self.linear_d = nn.Linear(in_feats, 7)
         self.linear_g = nn.Linear(in_feats, 6)
         self.linear_e = nn.Linear(in_feats, 3)
 
         self.base.reset_classifier(num_classes=0)
-
+        
     def forward(self, inputs):
         outputs = self.base(inputs)
         return (
@@ -25,3 +25,14 @@ class Model(nn.Module):
             self.linear_g(outputs),
             self.linear_e(outputs)
         )
+
+        
+if __name__ == "__main__":
+    from utils import get_config
+
+    config = get_config()
+    model = get_model(
+        config.model.name,
+        config.model.pretrained
+    )
+    print(model)
